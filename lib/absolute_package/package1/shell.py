8ab78b6ad1541a715c7a9fc427c8a5cd48a2606d
sam kariuki <samuel.kariuki1@student.moringaschool.com>
2023-08-21 21:48:58 +0300

    import lab done

---
+++ lib/absolute_package/package1/shell.py
@@ -0,0 +1,16 @@
+""" import os
+print(os.name)
+ """
+
+#if we want to import only a specific function or defination keyword `from` 
+
+""" from os import name
+print(name) """
+
+# the from keyword also allows us to import the whole  using asterisk symbol(*)
+
+""" from os import*
+print(name) """
+
+# ,however this is bad practice...can lead to misleading.
+
